b576786de8f808f3276bf2cdeb720f78f4ce4fa5
jck <jckleinbourg@gmail.com>
2024-02-25 00:32:39 +0100

    feat: Show list

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,23 @@
  * MAIN APP
  * --------
  */
-console.log(window.location.search);
+
 const list = window.location.search.substr(1).split(';');
-console.log(list);
+
+const container = document.querySelector('.container');
+list.forEach(str => {
+    const box = document.createElement('div');
+    box.classList.add('box');
+
+    const name = document.createElement('div');
+    name.classList.add('name');
+    name.innerHTML = decodeURIComponent(str);
+
+    const icon = document.createElement('div');
+    icon.classList.add('icon');
+    icon.innerHTML = "&#xf1821;";
+
+    box.appendChild(icon);
+    box.appendChild(name);
+    container.appendChild(box);
+});
